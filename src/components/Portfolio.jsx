c70647718195885abf2a0ceb6a1db6c4e41187a6
NasirHussain5226 <codingwithhussain@gmail.com>
2023-01-22 16:51:19 +0500

    Changes in portfolio section

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,7 +5,7 @@ import reactWeather from '../assets/Portfolio/reactweatherapp.png';
 
 const Portfolio = () => {
 
-  const portfolios =[
+  const portfolios = [
     {
       id: 1,
       src: reactWeather,
@@ -32,51 +32,30 @@ const Portfolio = () => {
       href: 'https://github.com/NasirHussain5226',
       href2: 'https://github.com/NasirHussain5226'
     },
-    {
-      id: 5,
-      src: reactWeather,
-      href: 'https://github.com/NasirHussain5226',
-      href2: 'https://github.com/NasirHussain5226'
-    },  {
-      id: 6,
-      src: reactWeather,
-      href: 'https://github.com/NasirHussain5226',
-      href2: 'https://github.com/NasirHussain5226'
-    },  {
-      id: 7,
-      src: reactWeather,
-      href: 'https://github.com/NasirHussain5226',
-      href2: 'https://github.com/NasirHussain5226'
-    },  {
-      id: 8,
-      src: reactWeather,
-      href: 'https://github.com/NasirHussain5226',
-      href2: 'https://github.com/NasirHussain5226'
-    },
   ];
   return (
     <div name='portfolio' className='bg-gradient-to-b from-black to-gray-700 text-white mx-auto w-full pb-12'>
       <div className='max-w-screenl-lg p-4 mx-auto flex flex-col justify-center w-full'>
-            <div className='pb-7 text-center'>
-                <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
-                <p className='py-5 '>Check out some of my work right here</p>
-            </div>
+        <div className='pb-7 text-center'>
+          <p className='text-4xl font-bold inline border-b-4 border-gray-500'>Portfolio</p>
+          <p className='py-5 '>Check out some of my work right here</p>
+        </div>
 
-            <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-8 px-10 sm:px-0'>
+        <div className='grid sm:grid-cols-2 md:grid-cols-4 gap-8 px-10 sm:px-0'>
 
-              {portfolios.map(({id, src, href, href2}) =>(
+          {portfolios.map(({ id, src, href, href2 }) => (
 
-              <div key={id} className='shadow-md shadow-gray-600 rounded-lg w-full'>
-              <img src={src} alt="Weather" className='hover:scale-105 duration-200 rounded-md cursor-pointer'/>
+            <div key={id} className='shadow-md shadow-gray-600 rounded-lg w-full'>
+              <img src={src} alt="Weather" className='hover:scale-105 duration-200 rounded-md cursor-pointer' />
               <div className='flex justify-center items-center'>
-                <a href= {href} className='w-1/2 px-5 py-2 flex justify-center m-3 duration-200 hover:scale-105'>Demo</a>
+                <a href={href} className='w-1/2 px-5 py-2 flex justify-center m-3 duration-200 hover:scale-105'>Demo</a>
                 <a href={href2} className='w-1/2 px-5 py-2 flex justify-center m-3 duration-200 hover:scale-105'>Code</a>
               </div>
-              </div>
-
-              ))}
-             
             </div>
+
+          ))}
+
+        </div>
       </div>
     </div>
   )
